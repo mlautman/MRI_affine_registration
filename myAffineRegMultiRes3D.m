9c60f27ce4644b0d36dc5f6c6ba55127a9ae26bc
vshreel <vshreel@seas.upenn.edu>
2015-03-10 19:05:19 -0400

    Updated 3.1.5

--- myAffineRegMultiRes3D.m
+++ myAffineRegMultiRes3D.m
@@ -1,35 +1,52 @@
+
 function [A,b] = myAffineRegMultiRes3D(I,J,iter,sigma)
-    %Number of levels
-    level = length(iter);
-    
-    %Initialize A and b
-    A = zeros(3,3);
-    b = zeros(3,1);
-    
-    for i = 1:level
-        %adjust [A,b] for next level
-%         A = 
-%         b = 
-        p = [reshape(A,9,1);b];
-        %skip levels with 0 iterations
-        if iter(i)>0
-            %gaussian LPF
-            coeff = 2^(level-i);
-            smoothedI = myGaussianLPF(I,coeff*sigma);
-            smoothedJ = myGaussianLPF(J,coeff*sigma);
 
-            %subsample images by 2
-            subsampI = smoothedI(1:2:end,1:2:end,1:2:end);
-            subsampJ = smoothedJ(1:2:end,1:2:end,1:2:end);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Objective function for Multi-resolution Affine Registration  %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% inputs - I,J,iter,sigma                                               %%
+%% I - fixed image                                                       %%
+%% J - moving image                                                      %%
+%% iter - row matrix specifying iterations at each level                 %%
+%% sigma - standard deviation of the Gaussian filter                     %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% outputs - A,b                                                         %%
+%% A - 3 x 3 rotation, scaling and shearing matrix                       %%
+%% b - 3 x 1 translation matrix                                          %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-            % Set options for optimization
-            options = optimset('GradObj','on','Display','iter','MaxIter',iter(i));
+% Number of levels
+level = length(iter);
 
-            % Run optimization
-            [p,fval] = fminunc(@(x)(myAffineObjective3D(x, subsampI, subsampJ)), p, options);
-            
-            A = [p(1:3) p(4:6) p(7:9)];
-            b = p(10:12);
-        end
+% Initialize A and b
+A = eye(3);
+b = zeros(3,1);
+
+p = [reshape(A,9,1);b];
+
+for i = 1:level
+    % skip levels with 0 iterations
+    if iter(i)>0
+        % Gaussian LPF
+        coeff = 2^(level-i);
+        smoothedI = myGaussianLPF(I,coeff*sigma);
+        smoothedJ = myGaussianLPF(J,coeff*sigma);
+        
+        % subsample images by 2^(level - i)
+        subsampI = smoothedI(1:coeff:end,1:coeff:end,1:coeff:end);
+        subsampJ = smoothedJ(1:coeff:end,1:coeff:end,1:coeff:end);
+        
+        % Set options for optimization
+        options = optimset('GradObj','on','Display','iter','MaxIter',iter(i));
+        
+        % Run optimization
+        [p,fval] = fminunc(@(x)(myAffineObjective3D(x, subsampI, subsampJ)), p, options);
     end
+    
+    A = [p(1:3) p(4:6) p(7:9)];
+    b = p(10:12);
+    
+    % Update p for next iteration
+    p = [reshape(A,9,1);2*b];
+end
 end
