82f4ab61bda68fe7657d597571db8bea413da16b
Mernies <jamesjwang@gmail.com>
2015-03-17 21:24:16 -0400

    Updated p322

--- p322.m
+++ p322.m
@@ -3,21 +3,44 @@
 
 clear;
 
-% initial image
-[averageimage,~] = myReadNifti('sub001_mri.nii');
+% initial test image
+[averageimage,spacing] = myReadNifti('sub021_mri.nii');
+
+% Create filename string parts
+% base1 = 'sub00';
+% base2 = 'sub0';
+base = 'sub0';
+ending = '_mri.nii';
+
+% Average all test image intensities
+for i = 2:20
+    % Create filename
+    imagenum = num2str(i+20);
+    filename = strcat(base,imagenum,ending);
+    % Read image and store in cell array
+    [currentimage,~] = myReadNifti(filename);
+%     image_cell{i} = currentimage;
+    % Add image to moving average
+    totalintensity = averageimage*(i-1);
+    averageimage = (totalintensity + currentimage)/i;
+end
+
+% Write to file
+myWriteNifti('Avgimgtest0_mri.nii',averageimage,spacing);
 
 % Create filename string parts
 base1 = 'sub00';
 base2 = 'sub0';
-ending = '_mri.nii';
+
+% Create 20x1 cell arrays
+A_cell = cell(20,1);
+b_cell = cell(20,1);
 
 % Create 20x1 cell arrays
 image_cell = cell(20,1);
 
-image_cell{1} = averageimage;
-
-% Average all training image intensities
-for i = 2:20
+% Populate training image cells
+for i = 1:20
     % Create filename
     imagenum = num2str(i);
     if i<10
@@ -25,67 +48,44 @@ for i = 2:20
     else
         filename = strcat(base2,imagenum,ending);
     end
-    % Read image and store in cell array
-    [currentimage,~] = myReadNifti(filename);
-    image_cell{i} = currentimage;
-    % Add image to moving average
-    totalintensity = averageimage*(i-1);
-    averageimage = (totalintensity + currentimage)/i;
+    % Read image and store in cell
+    [image_cell{i},~] = myReadNifti(filename);
 end
 
-% Write to file
-myWriteNifti('Avgimg0_mri.nii',averageimage,[80, 96, 80]);
+% Definte iteration vector and smoothing factor
+iter = [20, 10, 5];
+sigma = 1;
+% Initialize evaluation vector
+meanovl = zeros(5,1);
+path = 'F:\Class Stuff\Biomed Image Analysis\Grand Assignment\train';
 
 % Iterate 5 times
 for counts = 1:5
+    [A, b] = myAffineRegMultiRes3D(averageimage,image_cell{1},iter,sigma);
+    newaverage = myTransformImage(averageimage, image_cell{1}, A, b, 'linear');
+    A_cell{1} = A;
+    b_cell{1} = b;
     % Register each image to current average
-    % Initialize A and b
-    A = eye(3);
-    b = zeros(3,1);
-
-    p = [reshape(A,9,1);b];
-    % Gaussian LPF
-    sigma = 1;
-    smoothedaverage = myGaussianLPF(averageimage,2*sigma);
-    subsampavg = smoothedaverage(1:2:end,1:2:end,1:2:end);
-
-    % Create a struct object for storing gradient of moving image
-    g_struct = struct('dy',{},'dx',{},'dz',{});
-    for i = 1:20
-        % Optimize objective function
-        currentimage = image_cell{i};
-        smoothedcurrent = myGaussianLPF(currentimage,2*sigma);
-
-        % subsample images in each dimension by 2
-        subsampcurrent = smoothedcurrent(1:2:end,1:2:end,1:2:end);
-
-        % compute gradient of moving image
-        [g_struct(1).dy, g_struct(1).dx, g_struct(1).dz] = gradient(subsampcurrent);
-
-        % Set options for optimization
-        options = optimset('GradObj','on','Display','iter','MaxIter',50);
-
-        % Run optimization
-        [p,fval] = fminunc(@(x)(myAffineObjective3D(x, subsampavg, subsampcurrent, g_struct(1).dy, g_struct(1).dx, g_struct(1).dz)), p, options);
-        A = [p(1:3) p(4:6) p(7:9)];
-        b = p(10:12);
-
-        % Register image to average
-        image_cell{i} = myTransformImage(averageimage, currentimage, A, b, 'linear');
-    end
-    
-    % Compute new average
-    averageimage = image_cell{1};
     for i = 2:20
-        currentimage = image_cell{i};
-        % Add image to moving average
-        totalintensity = averageimage*(i-1);
-        averageimage = (totalintensity + currentimage)/i;
+        % Perform registration and transformation
+        [A, b] = myAffineRegMultiRes3D(averageimage,image_cell{i},iter,sigma);
+        transformedimage = myTransformImage(averageimage, image_cell{i}, A, b, 'linear');
+        A_cell{i} = A;
+        b_cell{i} = b;
+        
+        % Add to moving average
+        totalintensity = newaverage*(i-1);
+        newaverage = (totalintensity + transformedimage)/i;
     end
     
+    % Evaluate performance
+    meanovl(counts) = evaluateOnTrainingSet(path, A_cell, b_cell);
+       
     % Write averageimage(counts) to file
-    start = 'Avgimg';
+    start = 'Avgimgtest';
     ending = '_mri.nii';
     filename = strcat(start,num2str(counts),ending);
-    myWriteNifti(filename,averageimage,[80, 96, 80]);
+    myWriteNifti(filename,newaverage,spacing);
+    
+    averageimage = newaverage;
 end
