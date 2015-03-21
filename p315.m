eeadf4c2910a9df4762a56cc23e489de9df14d98
Mernies <jamesjwang@gmail.com>
2015-03-21 13:07:07 -0400

    Problem 3.1.5 (multi-resolution affine registration

---
+++ p315.m
@@ -0,0 +1,21 @@
+% problem 3.1.5
+% Multi-resolution affine registration
+
+clear;
+
+% load images
+[image1,spacing] = myReadNifti('sub021_mri.nii');
+[image2,spacing2] = myReadNifti('sub001_mri.nii');
+
+% Perform registration
+iter = [20,10,5];
+sigma = 1;
+[A,b] = myAffineRegMultiRes3D(image1,image2,iter,sigma);
+
+% output original results
+Aorig = eye(3,3);
+borig = zeros(3,1);
+myViewAffineReg(image1,image2,spacing,Aorig,borig);
+
+% output optimal results
+myViewAffineReg(image1,image2,spacing,A,b);
