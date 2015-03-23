63b55e9aa0c71b538d443be35f7e4d3761f20a13
vshreel <vshreel@seas.upenn.edu>
2015-03-23 14:10:07 -0400

    put everything in zip file

--- setup_env.m
+++ setup_env.m
@@ -1,6 +1,5 @@
 % Setup the environment
 addpath(genpath('./train/'))
 addpath(genpath('./test/'))
-addpath(genpath('./segmentation template')
 addpath(genpath('./NIFTI_20110921/'))
 addpath(genpath('./starter_code/'))
